7158c5ded18cbf6a705b8e7ad0c5275098ae87cb
julius legaspi (ph) <71057113+juliusL03@users.noreply.github.com>
2022-04-24 05:05:47 +0800

    enhance ui

--- client/src/components/loginForm.js
+++ client/src/components/loginForm.js
@@ -17,13 +17,30 @@ function LoginForm(){
     }
 
     return (
-        <div>
+        // <div>
+        //     <pre>{JSON.stringify(dataContent, undefined, 2)}</pre>
+        //     <input type='text' name='name' value={dataContent.name} onChange={changeHandle} />
+        //     <input type='email' name='email' value={dataContent.email} onChange={changeHandle} />
+        //     <input type='password' name='password' value={dataContent.password} onChange={changeHandle} />
+        // </div>
+       
+        <form>
             <pre>{JSON.stringify(dataContent, undefined, 2)}</pre>
-            <input name='name' value={dataContent.name} onChange={changeHandle} />
-            <input type='email' name='email' value={dataContent.email} onChange={changeHandle} />
-            <input type='password' name='password' value={dataContent.password} onChange={changeHandle} />
-        </div>
-        
+            <div className="form-group">
+                <label for="exampleInputName1">Name</label>
+                <input  className="form-control" id="exampleInputEmail1" placeholder="Enter name" name='name' value={dataContent.name} onChange={changeHandle} />
+                <small id="emailHelp" className="form-text text-muted">We'll never share your name with anyone else</small>
+            </div>
+            <div className="form-group">
+                <label className="exampleInputEmail1">Email</label>
+                <input  className="form-control" id="exampleInputEmail1" placeholder="Enter email" name='email' value={dataContent.email} onChange={changeHandle} />
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else</small>
+            </div>
+            <div className="form-group">
+                <label className="exampleInputPassword1">Password</label>
+                <input className="form-control" id="exampleInputPassword" type="password" name="password" value={dataContent.password} onChange={changeHandle} />
+            </div>
+        </form>
     );
 }
 
